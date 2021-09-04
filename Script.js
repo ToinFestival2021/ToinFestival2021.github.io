54f19dc500f7b45263f0ed3aa5f1d5a78b649a0a
ToinFestival2021 <88518382+ToinFestival2021@users.noreply.github.com>
2021-09-04 18:47:36 +0900

    Update Script.js

--- Script.js
+++ Script.js
@@ -14,3 +14,20 @@ typewriter({
     string: "9月11-12日、開催決定！！", //文字列
     speed: 200 //速度
 });
+
+const typewriter2 = (param) => {
+
+    let el = document.querySelector(param.el);
+    let speed = param.speed;
+    let string = param.string.split("");
+    string.forEach((char, index) => {
+        setTimeout(() => {
+            el.textContent += char;
+        }, speed * index);
+    });
+};
+typewriter2({
+    el: "#typewriter2", //要素
+    string: "第65回桐陰グランプリ開催！", //文字列
+    speed: 200 //速度
+});

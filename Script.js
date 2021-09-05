68fe4e24f0aed9dfc52bb4d199842cf69e45e720
uyTIVggXrA <tampopo256@gmail.com>
2021-09-06 01:16:35 +0900

    add / remove indent

--- Script.js
+++ Script.js
@@ -1,33 +1,31 @@
 const typewriter = (param) => {
-
-    let el = document.querySelector(param.el);
-    let speed = param.speed;
-    let string = param.string.split("");
-    string.forEach((char, index) => {
-        setTimeout(() => {
-            el.textContent += char;
-        }, speed * index);
-    });
+  let el = document.querySelector(param.el);
+  let speed = param.speed;
+  let string = param.string.split("");
+  string.forEach((char, index) => {
+    setTimeout(() => {
+      el.textContent += char;
+    }, speed * index);
+  });
 };
 typewriter({
-    el: "#typewriter", //要素
-    string: "9月11-12日、開催決定！！", //文字列
-    speed: 200 //速度
+  el: "#typewriter", //要素
+  string: "9月11-12日、開催決定！！", //文字列
+  speed: 200, //速度
 });
 
 const typewriter2 = (param) => {
-
-    let el = document.querySelector(param.el);
-    let speed = param.speed;
-    let string = param.string.split("");
-    string.forEach((char, index) => {
-        setTimeout(() => {
-            el.textContent += char;
-        }, speed * index);
-    });
+  let el = document.querySelector(param.el);
+  let speed = param.speed;
+  let string = param.string.split("");
+  string.forEach((char, index) => {
+    setTimeout(() => {
+      el.textContent += char;
+    }, speed * index);
+  });
 };
 typewriter2({
-    el: "#typewriter2", //要素
-    string: "第65回桐陰グランプリ開催！", //文字列
-    speed: 200 //速度
+  el: "#typewriter2", //要素
+  string: "第65回桐陰グランプリ開催！", //文字列
+  speed: 200, //速度
 });
